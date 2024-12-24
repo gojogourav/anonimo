ce87c59d824920a274aa4182cbb10cafc4edc605
unknown <gojogourav@gmail.com>
2024-12-24 23:55:43 +0530

    day1 end

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='h-screen w-full justify-center items-center flex bg-black text-white text-2xl'>
+        Onboarding
+    </div>
+  )
+}
+
+export default page
